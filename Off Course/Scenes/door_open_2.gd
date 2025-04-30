ee858adcd57ef2c0ade3eaad2796f5e2c7309a63
mgrispino <mgrispino@fordham.edu>
2025-04-30 12:23:40 -0400

    Trying to add moving saw & Level 4 additions

--- Off Course/Scenes/door_open_2.gd
+++ Off Course/Scenes/door_open_2.gd
@@ -13,4 +13,4 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 			call_deferred("_change_scene")
 
 func _change_scene():
-	get_tree().change_scene_to_file("res://Scenes/level_4.tscn")
+	get_tree().change_scene_to_file("res://Scenes/level 3.tscn")
