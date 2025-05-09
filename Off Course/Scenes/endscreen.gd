fb637e4c707b85a6591c8bc81dd279f48ff4f64f
mgrispino <mgrispino@fordham.edu>
2025-05-08 20:12:23 -0400

    final changes

--- Off Course/Scenes/endscreen.gd
+++ Off Course/Scenes/endscreen.gd
@@ -3,9 +3,13 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	timerUI.hide()
+	GameTimer.stop_timer()
+	GameTimer.reset_timer()
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_menu_pressed() -> void:
+	ConfirmSfx.play()
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	pass # Replace with function body.
--- Off Course/Scenes/switch.gd
+++ Off Course/Scenes/switch.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$"../FadetoBlack/AnimationPlayer".play("RESET")
 	pass # Replace with function body.
 
 
@@ -12,6 +13,7 @@ func _process(delta: float) -> void:
 	
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "CharacterBody2D":
+		$"../FadetoBlack/AnimationPlayer".play("fade_black")
 		await get_tree().create_timer(3.0).timeout  
 		get_tree().change_scene_to_file("res://Scenes/Endscreen.tscn")
 	pass # Replace with function body.
